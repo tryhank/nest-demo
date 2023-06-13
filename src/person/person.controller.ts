b40d89b2afba7b6728f7ad9cf1dfc8cb34a3b454
赵俊龙 <zhaojunlong@junlong-MacBook-Pro.local>
2023-06-13 10:06:12 +0800

    feat: 依赖注入方式

--- src/person/person.controller.ts
+++ src/person/person.controller.ts
@@ -9,6 +9,7 @@ import {
   Query,
   UseInterceptors,
   UploadedFiles,
+  Inject,
 } from '@nestjs/common';
 import { PersonService } from './person.service';
 import { CreatePersonDto } from './dto/create-person.dto';
@@ -17,7 +18,12 @@ import { AnyFilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('person')
 export class PersonController {
-  constructor(private readonly personService: PersonService) {}
+  constructor(
+    private readonly personService: PersonService,
+    @Inject('person2') private readonly person2,
+    @Inject('person3') private readonly person3,
+    @Inject('person4') private readonly person4,
+  ) {}
 
   // formdata 传参 Content-Type: multipart/form-data
   @Post('file')
@@ -42,12 +48,14 @@ export class PersonController {
   // query 传参
   @Get()
   findAll(@Query('name') name: string, @Query('age') age: number) {
+    console.log(this.person4, '111');
     return `name: ${name},age: ${age}`;
   }
 
   // url param 传参
   @Get(':id')
   findOne(@Param('id') id: string) {
+    console.log(this.person2);
     return this.personService.findOne(+id);
   }
 
--- src/person/person.module.ts
+++ src/person/person.module.ts
@@ -2,8 +2,39 @@ import { Module } from '@nestjs/common';
 import { PersonService } from './person.service';
 import { PersonController } from './person.controller';
 
+function asyncFn() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        name: 'abc',
+        age: 12,
+      });
+    }, 3000);
+  });
+}
+
 @Module({
   controllers: [PersonController],
-  providers: [PersonService],
+  providers: [
+    PersonService,
+    {
+      provide: 'person2',
+      useValue: 'aaabb',
+    },
+    {
+      provide: 'person3',
+      useFactory(name: string) {
+        console.log(name, 'name');
+        return '123';
+      },
+      inject: ['person2'],
+    },
+    {
+      provide: 'person4',
+      async useFactory(name: string) {
+        return await asyncFn();
+      },
+    },
+  ],
 })
 export class PersonModule {}
